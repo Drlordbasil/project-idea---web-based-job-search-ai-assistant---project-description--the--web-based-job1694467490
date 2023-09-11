928acf25ad41b033979b9dfce18d23fa6d7f9089
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-11 17:24:52 -0400

    Initial commit - main.py

---
+++ main.py
@@ -0,0 +1,96 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class JobSearchAIAssistant:
+    def __init__(self):
+        self.job_title = ""
+        self.location = ""
+        self.keywords = []
+        self.companies = []
+        self.job_postings = []
+        self.filtered_postings = []
+
+    def get_user_input(self):
+        self.job_title = input("Enter desired job title: ")
+        self.location = input("Enter location: ")
+
+        keywords_input = input("Enter optional keywords (separated by commas): ")
+        self.keywords = [keyword.strip() for keyword in keywords_input.split(",")]
+
+        companies_input = input("Enter optional companies (separated by commas): ")
+        self.companies = [company.strip() for company in companies_input.split(",")]
+
+    def scrape_job_postings(self):
+        job_sites = ["Indeed", "LinkedIn", "Glassdoor"]
+
+        for site in job_sites:
+            url = self.generate_job_site_url(site)
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, "html.parser")
+                job_postings = self.scrape_job_site(site, soup)
+                self.job_postings.extend(job_postings)
+            else:
+                print(f"Failed to scrape job postings from {site}")
+
+    def generate_job_site_url(self, site):
+        url = f"https://{site.lower()}.com/{self.job_title}/{self.location}"
+
+        if self.keywords:
+            keyword_param = "%20".join(self.keywords)
+            url += f"?keywords={keyword_param}"
+
+        if self.companies:
+            company_param = "%20".join(self.companies)
+            url += f"&companies={company_param}"
+
+        return url
+
+    def scrape_job_site(self, site, soup):
+        if site == "Indeed":
+            return self.scrape_indeed(soup)
+        elif site == "LinkedIn":
+            return self.scrape_linkedin(soup)
+        elif site == "Glassdoor":
+            return self.scrape_glassdoor(soup)
+
+    def scrape_indeed(self, soup):
+        job_postings = []
+        # TODO: Implement logic to scrape job postings from Indeed
+        # Use BeautifulSoup to find relevant tags and extract information from the soup object
+        # Append the extracted data to the job_postings list
+        return job_postings
+
+    def scrape_linkedin(self, soup):
+        job_postings = []
+        # TODO: Implement logic to scrape job postings from LinkedIn
+        # Use BeautifulSoup to find relevant tags and extract information from the soup object
+        # Append the extracted data to the job_postings list
+        return job_postings
+
+    def scrape_glassdoor(self, soup):
+        job_postings = []
+        # TODO: Implement logic to scrape job postings from Glassdoor
+        # Use BeautifulSoup to find relevant tags and extract information from the soup object
+        # Append the extracted data to the job_postings list
+        return job_postings
+
+    def filter_job_postings(self):
+        self.filtered_postings = [posting for posting in self.job_postings if self.criteria_met(posting)]
+
+    def criteria_met(self, posting):
+        # TODO: Implement logic to determine if a job posting meets the predefined criteria
+        # Return True or False based on the criteria
+        return True
+
+    def run_ai_assistant(self):
+        self.get_user_input()
+        self.scrape_job_postings()
+        self.filter_job_postings()
+
+
+if __name__ == "__main__":
+    assistant = JobSearchAIAssistant()
+    assistant.run_ai_assistant()
